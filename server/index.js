622de2eba1449442ae29ab1352f628fa82cf020f
erichen886 <erichen886@gmail.com>
2020-06-19 16:22:24 -0700

    api call

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const controllers = require('./controllers.js');
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/api/products', controllers.getProducts);
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
