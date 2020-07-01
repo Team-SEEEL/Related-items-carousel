0e7fea433c39d7739472cc066a1596bc6b4237f3
erichen886 <erichen886@gmail.com>
2020-07-01 12:24:23 -0700

    resolved issue with path.join

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const path = require ('path')
 const controllers = require('./controllers.js');
-
+const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "/../public")));
 
-const port = 3000;
+const port = 3001;
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.sendFile(`${__dirname}/../public/bundle.js`));
 
 app.get('/api/products', controllers.getProducts);
 app.get('/api/products/:index', controllers.getDepartment);
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
